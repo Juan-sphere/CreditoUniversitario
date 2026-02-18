35642e76c7e92b6687d649a917863b41af5b7c83
JoselinSphere <joselin.rivas@sphere.com.pe>
2026-02-18 11:25:37 -0500

    update frontend

--- frontend/app/components/NuevoRegistro.vue
+++ frontend/app/components/NuevoRegistro.vue
@@ -1,32 +1,44 @@
 <template>
-    <div>
-        <div class="flex items-center mt-5 border border-gray-300 text-center"
-            :class="{ 'justify-around w-full': tabs.length >= 3, 'w-fit': tabs.length < 3 }">
-            <div v-for="(tab, index) in tabs" :key="index" :class="[
-                'py-2 px-4',
-                { 'bg-[#F8FBF9]': index === 0, 'bg-[#DCE4E6]': index !== 0 }
-            ]" :style="tabs.length >= 3 ? { width: `${100 / tabs.length}%` } : {}">
-                <h2>{{ tab }}</h2>
-            </div>
-        </div>
-        <div>
-            <div class="bg-[#F8FBF9] p-2 border border-gray-300">
-                <p>
-                    {{ title }}
-                </p>
-            </div>
-            <div class="p-5 border border-gray-300">
-                <slot name="nuevo-registro"></slot>
-            </div>
-        </div>
+  <div class="mt-5">
+    <div
+      v-if="ifTabs"
+      class="flex items-center border border-gray-300 text-center"
+      :class="{
+        'justify-around w-full': tabs.length >= 3,
+        'w-fit': tabs.length < 3,
+      }"
+    >
+      <div
+        v-for="(tab, index) in tabs"
+        :key="index"
+        :class="[
+          'py-2 px-4',
+          { 'bg-[#F8FBF9]': index === 0, 'bg-[#DCE4E6]': index !== 0 },
+        ]"
+        :style="tabs.length >= 3 ? { width: `${100 / tabs.length}%` } : {}"
+      >
+        <h2>{{ tab }}</h2>
+      </div>
     </div>
+    <div>
+      <div class="bg-[#F8FBF9] p-2 border border-gray-300">
+        <p>
+          {{ title }}
+        </p>
+      </div>
+      <div class="p-5 border border-gray-300">
+        <slot name="nuevo-registro"></slot>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-defineProps<({
-    tabs: string[]
-    title: string
-})>()
+defineProps<{
+  tabs: string[];
+  title: string;
+  ifTabs?: boolean;
+}>();
 </script>
 
 <style scoped></style>
--- frontend/app/pages/informacion-socioeconomica/index.vue
+++ frontend/app/pages/informacion-socioeconomica/index.vue
@@ -1,3 +1,55 @@
 <template>
-    <h1>Información Socioeconómica</h1>
+  <div class="w-full">
+    <Register
+      :tabs="[
+        'Ingresos Familiares',
+        'Egresos Familiares',
+        'Balance Presupuestal',
+      ]"
+    />
+    <NuevoRegistro
+      :ifTabs="false"
+      :tabs="[]"
+      title="Ingresos Familiares Mensuales"
+    >
+      <template #nuevo-registro>
+        <div class="grid grid-cols-2 gap-4 p-2">
+          <div class="flex flex-col gap-2">
+            <label class="text-gray-700 font-semibold"
+              >Concepto <span class="text-red-500">(*)</span></label
+            >
+            <input
+              type="text"
+              class="w-full px-3 py-1 border border-gray-300 rounded"
+            />
+          </div>
+          <div class="flex flex-col gap-2">
+            <label class="text-gray-700 font-semibold"
+              >Monto en Soles(S/) <span class="text-red-500">(*)</span></label
+            >
+            <select class="w-full px-3 py-1 border border-gray-300 rounded">
+              <option>--Seleccione--</option>
+            </select>
+          </div>
+          <div class="flex flex-col gap-2">
+            <label class="text-gray-700 font-semibold"
+              >Aportante <span class="text-red-500">(*)</span></label
+            >
+            <input
+              type="text"
+              class="w-full px-3 py-1 border border-gray-300 rounded"
+            />
+          </div>
+        </div>
+        <div class="w-full flex justify-between items-center mt-5 pl-2">
+          <div>
+            <p class="text-white bg-red-500">Campos Obligatorios (*)</p>
+          </div>
+          <button type="submit" class="px-4 py-1 bg-terciary text-white">
+            Registrar
+          </button>
+        </div>
+      </template>
+    </NuevoRegistro>
+  </div>
 </template>
