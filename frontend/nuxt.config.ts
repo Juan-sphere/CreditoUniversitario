55211652fe2fe1ca2bc1b6ee1be3e6b67aaa5308
JoselinSphere <joselin.rivas@sphere.com.pe>
2026-02-19 11:29:46 -0500

    add maps

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,10 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-   vite: {
-    plugins: [...tailwindcss() as any],
-  }
-})
+  app: {
+    head: {
+      script: [
+        {
+          src: `https://maps.googleapis.com/maps/api/js?key=${process.env.NUXT_PUBLIC_GOOGLE_MAPS_KEY}&libraries=places`,
+          async: true,
+          defer: true,
+        },
+      ],
+    },
+  },
+  vite: {
+    plugins: [...(tailwindcss() as any)],
+  },
+});
